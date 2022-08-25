24f09f812d11d163f4f3f476b7f96a610ab44914
joaonotfound <joaonotfound@protonmail.com>
2022-08-24 23:35:33 -0300

    style: change header style

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,6 @@
 import { Header as MantineHeader } from "@mantine/core"
 import { Logo } from "../logo/logo"
 
-export  const Header = () => {
-    return <MantineHeader height='sm' ><Logo /></MantineHeader>
+export const Header = () => {
+    return <MantineHeader height={60} p="xs" ><Logo /></MantineHeader>
 }
