57214966427d614c5386d5d3150b7281bf4dd901
joaonotfound <joaonotfound@protonmail.com>
2022-08-24 23:36:13 -0300

    feat: add layout hoc to the app

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,7 @@
 import { AppShell, MantineThemeOverride, MantineProvider } from "@mantine/core"
 import { Header } from "../header/header"
+import { Layout } from "hoc";
+import { Text } from "@mantine/core";
 
 const globalTheme: MantineThemeOverride = {
   colorScheme: 'dark',
@@ -11,8 +13,10 @@ function App() {
   return (
     <MantineProvider theme={globalTheme} withGlobalStyles withNormalizeCSS>
       <AppShell
+        padding='md'
         header={<Header />}
       >
+        <Layout />
       </AppShell>
     </MantineProvider>
   );
