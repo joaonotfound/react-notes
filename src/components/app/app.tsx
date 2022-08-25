f57cf2ce2444aefb39484d484b78dade6fd454f8
joaonotfound <joaonotfound@protonmail.com>
2022-08-25 01:13:09 -0300

    refactor: make the router available throughout the entire application

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,6 +1,7 @@
 import { AppShell, MantineThemeOverride, MantineProvider } from "@mantine/core"
 import { Header } from "../header/header"
-import { Layout } from "hoc";
+import { Layout } from "hoc"
+import { BrowserRouter as Router } from "react-router-dom"
 
 const globalTheme: MantineThemeOverride = {
   colorScheme: 'dark',
@@ -10,13 +11,15 @@ const globalTheme: MantineThemeOverride = {
 
 export const App = () => {
   return (
-    <MantineProvider theme={globalTheme} withGlobalStyles withNormalizeCSS>
-      <AppShell
-        padding='md'
-        header={<Header />}
-      >
-        <Layout />
-      </AppShell>
-    </MantineProvider>
+    <Router>
+      <MantineProvider theme={globalTheme} withGlobalStyles withNormalizeCSS>
+        <AppShell
+          padding='md'
+          header={<Header />}
+        >
+          <Layout />
+        </AppShell>
+      </MantineProvider>
+    </Router>
   );
 }
--- src/hoc/layout/layout.tsx
+++ src/hoc/layout/layout.tsx
@@ -3,11 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 export const Layout = () => {
   return <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<p>homepage</p>} />
-        <Route path="/login" element={<AuthPage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<p>homepage</p>} />
+      <Route path="/login" element={<AuthPage />} />
+    </Routes>
   </>
 }
