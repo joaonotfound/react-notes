01f92fc2c4df4b3d7d92d56c3d64afb50ea15d4f
joaonotfound <joaonotfound@protonmail.com>
2022-08-25 00:33:02 -0300

    feat: create the auth page

---
+++ src/pages/auth/auth-page.tsx
@@ -0,0 +1,10 @@
+import { AuthCard } from "components"
+import { Center } from "@mantine/core"
+
+export const AuthPage = () => {
+  return <>
+    <Center>
+      <AuthCard />
+    </Center>
+  </>
+}
