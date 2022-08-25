b27d218f8edaf5f94798f3285e7fc571008ea728
joaonotfound <joaonotfound@protonmail.com>
2022-08-24 23:38:30 -0300

    refactor: make app a anonymous function

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,7 +1,6 @@
 import { AppShell, MantineThemeOverride, MantineProvider } from "@mantine/core"
 import { Header } from "../header/header"
 import { Layout } from "hoc";
-import { Text } from "@mantine/core";
 
 const globalTheme: MantineThemeOverride = {
   colorScheme: 'dark',
@@ -9,7 +8,7 @@ const globalTheme: MantineThemeOverride = {
   primaryColor: "blue"
 }
 
-function App() {
+export const App = () => {
   return (
     <MantineProvider theme={globalTheme} withGlobalStyles withNormalizeCSS>
       <AppShell
@@ -21,5 +20,3 @@ function App() {
     </MantineProvider>
   );
 }
-
-export default App;
