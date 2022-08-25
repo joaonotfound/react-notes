f5e512a3a28d7fb9beacc37ca9a15069e93aae45
joaonotfound <joaonotfound@protonmail.com>
2022-08-25 01:13:56 -0300

    feat: add some functionalities to the header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,18 @@
-import { Header as MantineHeader } from "@mantine/core"
-import { Logo } from "../logo/logo"
+import { Button, Header as MantineHeader, Group } from "@mantine/core"
+import { Logo } from "components"
+import { Link } from 'react-router-dom'
 
 export const Header = () => {
-    return <MantineHeader height={60} p="xs" ><Logo /></MantineHeader>
+    return <MantineHeader height={56} p="xs">
+        <Group position="apart" >
+            <Link to="/">
+                <Logo />
+            </Link>
+            <Group>
+                <Link to='/login'>
+                    <Button variant='light'> Log-in </Button>
+                </Link>
+            </Group>
+        </Group>
+    </MantineHeader>
 }
