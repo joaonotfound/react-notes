b41200727c6b865e92925f2c03b28b67556bdba7
joaonotfound <joaonotfound@protonmail.com>
2022-08-25 01:14:39 -0300

    refactor: remove unused imports

--- src/hoc/layout/layout.tsx
+++ src/hoc/layout/layout.tsx
@@ -1,5 +1,5 @@
 import { AuthPage } from "pages/auth/auth-page"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 
 export const Layout = () => {
   return <>
