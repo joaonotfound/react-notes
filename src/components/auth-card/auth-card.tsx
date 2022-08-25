7028426467f600415a08c610f25aff67777cb1d4
joaonotfound <joaonotfound@protonmail.com>
2022-08-25 00:42:08 -0300

    style: restyle the auth-card

--- src/components/auth-card/auth-card.tsx
+++ src/components/auth-card/auth-card.tsx
@@ -1,16 +1,15 @@
 import { Card, PasswordInput, Text, TextInput, Button, Group, Anchor, Divider } from "@mantine/core"
 import { useToggle } from "@mantine/hooks"
 import { FcGoogle } from 'react-icons/fc'
+import { MdAlternateEmail } from 'react-icons/md'
 
 export const AuthCard = () => {
   const [type, toggleType] = useToggle(['login', 'register']);
 
   return <>
     <Card withBorder
-      style={{
-        maxWidth: 400
-      }}
       radius="sm"
+      p='xl'
     >
       <Text size='xl' mb="md" weight={700}>Welcome!</Text>
 
@@ -22,13 +21,13 @@ export const AuthCard = () => {
 
       <form onSubmit={() => { }}>
         {type === 'register' && (
-          <TextInput required label="Username" placeholder="Insert your username" />
+          <TextInput required label="Username" placeholder="@username" />
         )}
-        <TextInput required label="Email" placeholder="your better email" />
-        <PasswordInput required label='Password' placeholder="your most secure password" />
+        <TextInput required label="Email" rightSection={<MdAlternateEmail />} placeholder="email" />
+        <PasswordInput required label='Password' placeholder="password" />
 
         <Group position="apart" mt='xl'>
-          <Anchor onClick={() => toggleType()}>
+          <Anchor size='sm' onClick={() => toggleType()}>
             {type === 'login' ? "Don't have an account yet?" : "Already have an account?"}
           </Anchor>
           <Button type="submit">  {type === 'login' ? "Login" : "Sign up"}</Button>
