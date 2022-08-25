8febb92d78918a824b41425d1a801a1fc7e8efab
joaonotfound <joaonotfound@protonmail.com>
2022-08-25 11:44:08 -0300

    feat: make the google sign in button functional

--- src/components/auth-card/auth-card.tsx
+++ src/components/auth-card/auth-card.tsx
@@ -1,11 +1,12 @@
 import { Card, PasswordInput, Text, TextInput, Button, Group, Anchor, Divider } from "@mantine/core"
 import { useToggle } from "@mantine/hooks"
+import { UserAuthentication } from "adapters"
 import { FcGoogle } from 'react-icons/fc'
 import { MdAlternateEmail } from 'react-icons/md'
 
 export const AuthCard = () => {
+  const userAuth = new UserAuthentication()
   const [type, toggleType] = useToggle(['login', 'register']);
-
   return <>
     <Card withBorder
       radius="sm"
@@ -14,7 +15,7 @@ export const AuthCard = () => {
       <Text size='xl' mb="md" weight={700}>Welcome!</Text>
 
       <Group grow mb='md'>
-        <Button variant="default" radius='lg' leftIcon={<FcGoogle />}> Sign in with google </Button>
+        <Button variant="default" radius='lg' leftIcon={<FcGoogle />} onClick={() => userAuth.signInWithGoogle()}> Sign in with google </Button>
       </Group>
 
       <Divider labelPosition="center" my='sm' label="or sign with email"></Divider>
