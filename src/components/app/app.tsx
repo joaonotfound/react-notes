9044b4b42a65ef81717f9e364a57830b84d8116a
joaonotfound <joaonotfound@protonmail.com>
2022-08-26 16:11:00 -0300

    feat: add redux provider to the app component

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -2,6 +2,8 @@ import { AppShell, MantineThemeOverride, MantineProvider } from "@mantine/core"
 import { Header } from "../header/header"
 import { Layout } from "hoc"
 import { BrowserRouter as Router } from "react-router-dom"
+import { Store } from "redux/authService"
+import { Provider } from "react-redux"
 
 const globalTheme: MantineThemeOverride = {
   colorScheme: 'dark',
@@ -12,14 +14,16 @@ const globalTheme: MantineThemeOverride = {
 export const App = () => {
   return (
     <Router>
-      <MantineProvider theme={globalTheme} withGlobalStyles withNormalizeCSS>
-        <AppShell
-          padding='md'
-          header={<Header />}
-        >
-          <Layout />
-        </AppShell>
-      </MantineProvider>
+      <Provider store={Store}>
+        <MantineProvider theme={globalTheme} withGlobalStyles withNormalizeCSS>
+          <AppShell
+            padding='md'
+            header={<Header />}
+          >
+            <Layout />
+          </AppShell>
+        </MantineProvider>
+      </Provider>
     </Router>
   );
 }
