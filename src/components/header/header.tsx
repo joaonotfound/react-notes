13ebca06e1df1f47eb08cc5883984824f2374927
joaonotfound <joaonotfound@protonmail.com>
2022-08-26 16:15:27 -0300

    feat: do not show login button if authenticated

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,17 +1,23 @@
 import { Button, Header as MantineHeader, Group } from "@mantine/core"
 import { Logo } from "components"
 import { Link } from 'react-router-dom'
+import { useAppSelector } from "redux/authService"
 
 export const Header = () => {
+    const authenticated = useAppSelector(state => state.authentication.isAuthenticated)
     return <MantineHeader height={56} p="xs">
         <Group position="apart" >
             <Link to="/">
                 <Logo />
             </Link>
             <Group>
-                <Link to='/login'>
-                    <Button variant='light'> Log-in </Button>
-                </Link>
+                {
+                    !authenticated && (
+                        <Link to='/login'>
+                            <Button variant='light'> Log-in </Button>
+                        </Link>
+                    )
+                }
             </Group>
         </Group>
     </MantineHeader>
