ba3980b6f1e250dc8e755935933300336147a8e8
joaonotfound <joaonotfound@protonmail.com>
2022-08-27 14:29:34 -0300

    feat: display feedpage on / if authenticated

--- src/hoc/layout/layout.tsx
+++ src/hoc/layout/layout.tsx
@@ -1,10 +1,17 @@
 import { AuthPage } from "pages/auth/auth-page"
+import { FeedPage } from "pages/feed/feed-page"
+import { HomePage } from "pages/homepage/homepage"
 import { Route, Routes } from "react-router-dom"
-
+import { useAppSelector } from "redux/authService"
 export const Layout = () => {
+  const authenticated = useAppSelector(state => state.authentication.isAuthenticated)
   return <>
     <Routes>
-      <Route path="/" element={<p>homepage</p>} />
+      <Route path="/" element={
+        authenticated
+          ? <FeedPage />
+          : <HomePage />
+      } />
       <Route path="/login" element={<AuthPage />} />
     </Routes>
   </>
