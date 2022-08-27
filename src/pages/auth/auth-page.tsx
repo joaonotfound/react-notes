be7eed92d23d2cf65cf121650271328ec9f4cde9
joaonotfound <joaonotfound@protonmail.com>
2022-08-27 13:50:21 -0300

    feat: when logging in redirect tot he homepage

--- src/pages/auth/auth-page.tsx
+++ src/pages/auth/auth-page.tsx
@@ -2,12 +2,16 @@ import { AuthCard } from "components"
 import { Center } from "@mantine/core"
 import { User } from "interfaces/user-interface"
 import { setAuthValue, setUserValue, useAppDispatch } from "redux/authService"
+import { useNavigate } from "react-router-dom"
 
 export const AuthPage = () => {
+  const navigate = useNavigate()
   const dispatch = useAppDispatch()
   const onAuthentication = (user: User) => {
     dispatch(setAuthValue(true))
     dispatch(setUserValue(user))
+    navigate('/')
+
   }
   return <>
     <Center>
