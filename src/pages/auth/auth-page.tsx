377f1d4d72e8d43e31c46ad699da27fce5ba67a9
joaonotfound <joaonotfound@protonmail.com>
2022-08-27 11:46:17 -0300

    feat: implement the onAuthentication method

--- src/pages/auth/auth-page.tsx
+++ src/pages/auth/auth-page.tsx
@@ -1,10 +1,17 @@
 import { AuthCard } from "components"
 import { Center } from "@mantine/core"
+import { User } from "interfaces/user-interface"
+import { setAuthValue, setUserValue, useAppDispatch } from "redux/authService"
 
 export const AuthPage = () => {
+  const dispatch = useAppDispatch()
+  const onAuthentication = (user: User) => {
+    dispatch(setAuthValue(true))
+    dispatch(setUserValue(user))
+  }
   return <>
     <Center>
-      <AuthCard />
+      <AuthCard onAuthentication={onAuthentication} />
     </Center>
   </>
 }
