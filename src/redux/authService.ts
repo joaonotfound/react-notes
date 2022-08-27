425afa9452556d6ab6c7912877867b04fd7ee2c7
joaonotfound <joaonotfound@protonmail.com>
2022-08-27 11:42:27 -0300

    feat: create the sign up method

--- src/redux/authService.ts
+++ src/redux/authService.ts
@@ -1,8 +1,9 @@
 
-import { AnyAction, configureStore  } from "@reduxjs/toolkit" 
+import { AnyAction, configureStore } from "@reduxjs/toolkit"
 import AuthStore from 'redux/store';
-import { useDispatch, useSelector, TypedUseSelectorHook} from "react-redux"
+import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux"
 import { ThunkAction } from "@reduxjs/toolkit";
+import { User } from "interfaces/user-interface";
 
 export const Store = configureStore({
     reducer: {
@@ -18,8 +19,14 @@ export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 
 const actions = AuthStore.actions
 
-export const setAuthValue=(isAuthenticated: boolean): ThunkAction<void, RootState, unknown, AnyAction> => {
+export const setAuthValue = (isAuthenticated: boolean): ThunkAction<void, RootState, unknown, AnyAction> => {
     return (dispatch, getState) => {
         dispatch(actions.setAuth({ isAuthenticated }))
+    }
+}
+
+export const setUserValue = (user: User) => {
+    return (dispatch: any, _: any) => {
+        dispatch(actions.setUser(user))
     }
 }
