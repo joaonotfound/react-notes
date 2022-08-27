c6572d1cdba63303d368c093b35d126a7263be8e
joaonotfound <joaonotfound@protonmail.com>
2022-08-27 15:54:33 -0300

    style: give a better look to the header logo

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,16 +1,18 @@
-import { Button, Header as MantineHeader, Group } from "@mantine/core"
+import { Button, Header as MantineHeader, Group, Box } from "@mantine/core"
 import { Logo } from "components"
 import { Link } from 'react-router-dom'
 import { useAppSelector } from "redux/authService"
 import { HeaderAvatar } from "./header-avatar"
+import { useNavigate } from "react-router-dom"
 
 export const Header = () => {
     const authenticated = useAppSelector(state => state.authentication.isAuthenticated)
+    const navigate = useNavigate()
     return <MantineHeader height={56} p="xs">
         <Group position="apart" >
-            <Link to="/">
+            <div onClick={() => navigate('/')}>
                 <Logo />
-            </Link>
+            </div>
             <Group>
                 {
                     !authenticated
@@ -21,5 +23,5 @@ export const Header = () => {
                 }
             </Group>
         </Group>
-    </MantineHeader>
+    </MantineHeader >
 }
--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,5 +1,17 @@
-import { BiNotepad } from 'react-icons/bi'
+import { Text } from '@mantine/core'
+import { createStyles } from '@mantine/core';
 
 export const Logo = () => {
-    return <BiNotepad size={24} ></BiNotepad>
+    const useStyle = createStyles(theme => ({
+        "logo": {
+            fontSize: 18,
+            letterSpacing: 1.05,
+            color: "gray",
+            ":hover": {
+                "cursor": 'pointer'
+            }
+        }
+    }))
+    const { classes } = useStyle();
+    return <Text weight="bold" className={classes.logo} color={'blue'} >Notes</Text>
 }
