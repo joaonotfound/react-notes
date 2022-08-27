72f31597dd59091fbdfc170165252b0c3fd91ebb
joaonotfound <joaonotfound@protonmail.com>
2022-08-27 14:16:54 -0300

    feat: create the header avatar component

--- src/components/header/header-avatar.tsx
+++ src/components/header/header-avatar.tsx
@@ -1,13 +1,14 @@
 import { Avatar, Menu } from "@mantine/core"
 import { IconSettings } from '@tabler/icons'
-
+import { useAppSelector } from "redux/authService"
 export const HeaderAvatar = () => {
+  const username = useAppSelector(state => state.authentication.user?.username)
   return <Menu
     shadow="sm"
     radius={0}
   >
     <Menu.Target>
-      <Avatar color="blue" radius={100}>JK</Avatar>
+      <Avatar color="blue" radius={100}>{username?.at(0)}</Avatar>
     </Menu.Target>
     <Menu.Dropdown>
       <Menu.Label>Config</Menu.Label>
