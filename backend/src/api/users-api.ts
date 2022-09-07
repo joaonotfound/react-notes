927408555638c0d03c9292c1e0972fb70228d864
joaonotfound <joaonotfound@protonmail.com>
2022-09-07 15:59:11 -0300

    feat: implement create-user method on api

--- backend/src/api/users-api.ts
+++ backend/src/api/users-api.ts
@@ -1,6 +1,7 @@
 import * as web from 'express-decorators'
 import { Request, Response } from 'express'
 import { UserDatabaseModel } from '@/data/models/user-database-model'
+import { CreateUserInterface } from '@/interfaces';
 
 @web.basePath('/users')
 export class UsersApi {
@@ -13,5 +14,22 @@ export class UsersApi {
     await this.usersDatabase.getUserBy(uid)
       .then(response => res.status(200).send(response))
       .catch(_ => res.status(400).send("User doesn't exist."))
+  }
+  @web.post('/create-user')
+  async createUserOnDatabse(req: Request, res: Response) {
+    const requestParams = req.query
+    if (requestParams.name === undefined) {
+      res.status(400).send("Missing name value")
+    }
+    if (requestParams.uid === undefined) {
+      res.status(400).send("Missing uid value")
+    }
+    const params: CreateUserInterface = {
+      name: requestParams.name as string,
+      uid: requestParams.uid as string
+    }
+    await this.usersDatabase.createUserOnDatabase(params)
+      .catch(err => res.status(400).send(err))
+      .then(createdUser => res.status(200).send(createdUser))
   }
 }
