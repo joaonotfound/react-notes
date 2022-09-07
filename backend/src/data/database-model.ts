6693ec1f33a777b6ed42618c943c449a9e7eb0ce
joaonotfound <joaonotfound@protonmail.com>
2022-09-07 00:14:04 -0300

    feat: create isValidToken method

--- backend/src/data/database-model.ts
+++ backend/src/data/database-model.ts
@@ -1,4 +1,5 @@
 
-export abstract class DatabaseModel{
+export abstract class DatabaseModel {
     public abstract verifyToken(idToken: string): Promise<Boolean>
+    public abstract isValidToken(idToken: string): Promise<Boolean>
 }
