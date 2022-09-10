a0e679c9f5fe87c4f19db74a591796cafb1fd785
joaonotfound <joaonotfound@protonmail.com>
2022-09-09 21:23:54 -0300

    refactor: rename auth-error-codes file

--- frontend/src/adapters/index.ts
+++ frontend/src/adapters/index.ts
@@ -1,2 +1,2 @@
 export { UserAuthentication } from "./user-authentication/user-authentication";
-export { AuthErrorsCodes } from './user-authentication/authErrorCodes'
+export { AuthErrorsCodes } from './user-authentication/auth-error-codes'
