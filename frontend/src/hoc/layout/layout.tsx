02ebf2a3db6f62688d87595bc93846a7638a6dfb
joaonotfound <joaonotfound@protonmail.com>
2022-09-10 18:54:23 -0300

    feat: automatically change auth state

--- frontend/src/hoc/layout/layout.tsx
+++ frontend/src/hoc/layout/layout.tsx
@@ -2,9 +2,14 @@ import { AuthPage } from "pages/auth/auth-page"
 import { FeedPage } from "pages/feed/feed-page"
 import { HomePage } from "pages/homepage/homepage"
 import { Route, Routes } from "react-router-dom"
-import { useAppSelector } from "redux/authService"
+import { setAuthValue, useAppSelector } from 'redux/authService'
+import { tokenAuth } from 'adapters/token-auth/token-auth'
+import { useAppDispatch } from "redux/authService"
+
 export const Layout = () => {
   const authenticated = useAppSelector(state => state.authentication.isAuthenticated)
+  const dispatch = useAppDispatch();
+  tokenAuth.isAuthenticated().then(() => dispatch(setAuthValue(true)))
   return <>
     <Routes>
       <Route path="/" element={
